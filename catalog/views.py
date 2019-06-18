9f6bc2dc04de03ec2224aebfb982f6218177d033
DJ Knox <dhknox@gmail.com>
2019-06-18 13:04:14 -0400

    add additional model queries to index template

--- catalog/views.py
+++ catalog/views.py
@@ -9,6 +9,11 @@ def index(request):
     # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
+
+    # Modify the view to generate counts for genres and books that contain a particular word (case insensitive)
+    num_books_with_banana_in_the_title = Book.objects.filter(title__icontains='banana').count()
+    num_genres_with_fiction_in_the_name = Genre.objects.filter(name__icontains='fiction').count()
+
     
     # Available books (status = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
@@ -21,6 +26,8 @@ def index(request):
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_books_with_banana_in_the_title': num_books_with_banana_in_the_title,
+        'num_genres_with_fiction_in_the_name': num_genres_with_fiction_in_the_name
     }
 
     # Render the HTML template index.html with the data in the context variable
