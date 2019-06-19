dfaaaff123c2065a0408f1e75896b29d98ebf087
DJ Knox <dhknox@gmail.com>
2019-06-19 17:18:05 -0400

    added borrower relationship between BookInstance and User models

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -48,6 +48,7 @@ urlpatterns += [
 
 
 from django.conf import settings
+from django.conf.urls import include, url  # For django versions before 2.0
 from django.urls import include, path  # For django versions from 2.0 and up
 
 if settings.DEBUG:
