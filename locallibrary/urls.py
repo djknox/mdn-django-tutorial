fd8917c27126bb2c9e37feddbcfa7b2aa5b132fb
DJ Knox <dhknox@gmail.com>
2019-06-19 11:11:57 -0400

    add django-debug-toolbar

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -39,3 +39,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+from django.conf import settings
+from django.urls import include, path  # For django versions from 2.0 and up
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+
+        # For django versions before 2.0:
+        # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
