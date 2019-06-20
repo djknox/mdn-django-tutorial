0733fc92c204911b4cbab07dd3208e29b802d535
DJ Knox <dhknox@gmail.com>
2019-06-20 07:31:20 -0400

    add form for a librarian to renew a book and generic forms for creating, updating, and deleting an author

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,5 +14,15 @@ urlpatterns += [
 ]
 
 urlpatterns += [   
-    path('borrowed/', views.LibrarianLoanedBooksByUserListView.as_view(), name='librarian-borrowed'),
+    path('borrowed/', views.LibrarianLoanedBooksByUserListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [   
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [  
+    path('author/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
 ]
